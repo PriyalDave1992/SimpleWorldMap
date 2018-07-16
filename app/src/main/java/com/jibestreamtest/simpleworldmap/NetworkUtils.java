e75209672c35854ea251768cdad36ca77e68263b
PriyalDave <priyaldave1992@gmail.com>
2018-07-16 09:11:12 -0400

    Simple World Map - Added internet permission dialog

---
+++ app/src/main/java/com/jibestreamtest/simpleworldmap/NetworkUtils.java
@@ -0,0 +1,39 @@
+package com.jibestreamtest.simpleworldmap;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.provider.Settings;
+
+/**
+ * Created by Priyal Divakala on 16/07/2018.
+ */
+
+class NetworkUtils {
+
+
+    static boolean haveNetworkConnection(Context context) {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = new NetworkInfo[0];
+        if (cm != null) {
+            netInfo = cm.getAllNetworkInfo();
+        }
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+        }
+        return haveConnectedWifi || haveConnectedMobile;
+    }
+
+
+}
